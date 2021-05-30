6c7ede4359002992d187dfe8805262edbaf2cf46
Denis Federico Zelaya <48973732+DenisFZelaya@users.noreply.github.com>
2021-05-29 20:53:52 -0700

    Eliminar db

--- routes/productos.js
+++ routes/productos.js
@@ -1,16 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../conexion/conexion');
 
 /* GET productos page. */
 router.get('/', function(req, res, next) {
-
-    db.query("SELECT * FROM tblproductos", function(err, resultado){
-        console.log(resultado);
-        res.render('productos', { 
-            title: 'Descarga tus libros.', Productos: resultado});
-    });
-
+    res.render('productos', { 
+        title: 'Descarga tus libros.'});
 })
 
 module.exports = router;
